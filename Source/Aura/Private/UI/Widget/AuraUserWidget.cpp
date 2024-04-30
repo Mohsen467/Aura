297ffba0fb12fc81da949e18cd94c2107029b191
Mohsen Sadeghi <mohsen_sadeghi69@yahoo.com>
2024-04-30 21:59:25 +0330

    Custom User Widget, Widget Controller and Progress Bar

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Mohsen Sadeghi
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,4 @@
+// Copyright Mohsen Sadeghi
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
